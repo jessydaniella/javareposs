f8ce1aeb4cf3f091c8b946160be1d64774c3fa4b
jessydaniella <jessydaniella2014@yahoo.com>
2021-07-02 11:58:54 +0100

    i added index2.html script2.js

---
+++ script2.js
@@ -0,0 +1,10 @@
+const number = prompt('Please Enter a Number:');
+let squareRoot = Math.sqrt(number);
+document.getElementById('sqr').innerHTML = 'The square Root of ${number} is ${squareroot}';
+    console.log(squareRoot)
+
+    const base = prompt('Enter the base triangle :')
+    const height = prompt('Enter the height triangle :')
+    let Area = 1/2 * base * height ;
+    console.log('The area is ', Area);
+    
